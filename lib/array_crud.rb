c954aef4495e98b054a591e551e4780a340eb006
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-13 21:36:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Harry", "Ron", "Hermoine", "Ginny"]
+  array = ["Harry", "Ron", "Hermoine", "Ginny"]
 end
 
 def add_element_to_end_of_array(array, element)
